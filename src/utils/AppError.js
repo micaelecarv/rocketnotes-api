ef6005be82765f2e615bee8f9933a0ede07803f9
Micaele Carvalho <micaelecarv@gmail.com>
2023-03-29 08:58:53 -0300

    feat: add upload files and save in the database

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -8,26 +8,4 @@ class AppError {
   }
 }
 
-function removeItemOnce(arr, value) {
-  var index = arr.indexOf(value);
-  if (index > -1) {
-    arr.splice(index, 1);
-  }
-  return arr;
-}
-
-function removeItemAll(arr, value) {
-  var i = 0;
-  while (i < arr.length) {
-    if (arr[i] === value) {
-      arr.splice(i, 1);
-    } else {
-      ++i;
-    }
-  }
-  return arr;
-}
-console.log(removeItemOnce([2, 5, 9, 1, 5, 8, 5], 5))
-console.log(removeItemAll([2, 5, 9, 1, 5, 8, 5], 5))
-
 module.exports = AppError;
