bfff49ebb9b3dc50896448fc1059c6a1f9dd868f
Micaele Carvalho <micaelecarv@gmail.com>
2023-04-03 01:49:50 -0300

    feat: changed env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv');
 require('express-async-errors')
 
 const migrationsRun = require('./database/sqlite/migrations')
@@ -33,5 +34,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
